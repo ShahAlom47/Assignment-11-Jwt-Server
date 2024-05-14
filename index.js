b1585068d6db1667e304400062754015f771ea20
sah alom <sahalom4729@gmail.com>
2024-05-14 18:05:00 +0400

    added url

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors({
-  origin: ["http://localhost:5173", `http://localhost:5174`],
+  origin: ["http://localhost:5173", `http://localhost:5174`,'https://assignmet-11-jwt.web.app','https://assignmet-11-jwt.firebaseapp.com'],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true,
 }
@@ -50,6 +50,12 @@ const verifyToken = (req, res, next) => {
   })
 }
 
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+}
+
 async function run() {
   try {
     // // Connect the client to the server	(optional starting in v4.7)
@@ -66,25 +72,17 @@ async function run() {
     app.post('/jwt', async (req, res) => {
       const userEmail = req.body
       const token = jwt.sign(userEmail, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
-      res.cookie('token', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production" ? true : false,
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-      })
+      res.cookie('token', token, cookieOptions)
       
       res.send({ success: true })
 
     })
 
-    //  clear cookes 
+    //  clear cookies
+    
     app.post('/jwt/logout', async (req, res) => {
       const userEmail = req.body;
-      res.clearCookie('token', {
-        maxAge: 0,
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production" ? true : false,
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-      })
+      res.clearCookie('token', { ...cookieOptions, maxAge: 0 })
 
       res.send({ success: true })
     });
